7d9e122e2c162dc4387fcaaf49d6ca9f14123957
IpBlDiT <dimokhot@hotmail.com>
2017-09-21 07:54:49 -0400

    Initial Commit

---
+++ Languages/Languages.cs
@@ -0,0 +1,126 @@
+﻿using CommonExtensions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HedgeHog {
+  public class Languages<TTest> where TTest : Languages<TTest>, new() {
+    public enum Language { en, es, pt };
+    public class Message {
+      public string Text { get; set; }
+      public string Source { get; set; }
+      public Message(string text, string source) {
+        this.Text = text;
+        this.Source = source;
+      }
+      public override string ToString() {
+        return new { Text, Source } + "";
+      }
+    }
+    private static readonly TTest instance = new TTest();
+    public static TTest Instance { get { return instance; } }
+    static string GetExecPath(string fileName) {
+      var suffix = System.IO.Path.Combine("App_Data", fileName);
+      var rootPath = System.Web.Hosting.HostingEnvironment.MapPath("~/");
+      if (string.IsNullOrWhiteSpace(rootPath)) return Helpers.GetExecPath(suffix);
+      return System.IO.Path.Combine(rootPath, suffix);
+    }
+    public static ExpandoObject RunTest() {
+      Get(Language.en);
+      Func<Message, object> text = m => {
+        var lines = (m.Text ?? "").Split('\n');
+        return lines.Length < 2 ? (object)m.Text : lines;
+      };
+      var dict = SetInternal(Instance).Select(kv => new { Lang = kv.Key + "", Text = text(kv.Value), kv.Value.Source }).ToArray();
+      return new { type = typeof(TTest), dict }.ToExpando();
+    }
+
+    protected Dictionary<Language, Message> _dict;
+    static Dictionary<string, string> _langsMap = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase) {
+        { "english","en"},
+        { "Español","es"},
+        { "spanish","es"},
+        { "Portuguese","es"},
+        { "Português","pt"}
+      };
+    public static string Get(string language) {
+      if (language.Length > 2) {
+        string l;
+        if (!_langsMap.TryGetValue(language, out l))
+          throw new Exception(new { language, message = "Is not mapped" } + "");
+        language = l;
+      }
+      Language e;
+      if (!Enum.TryParse(language, true, out e))
+        throw new Exception((new { language, message = "Is not of " + e.GetType().Name + " type" } + ""));
+      return Get(e);
+    }
+    public static string Get(Language lang) {
+      try {
+        return SetInternal(Instance)[lang].Text;
+      } catch (Exception exc) {
+        var typeName = new { type = typeof(TTest).FullName } + "";
+        throw new Exception(typeName, exc);
+      };
+    }
+    static Func<TTest, Dictionary<Language, Message>> SetInternal = i => i._dict ?? (i._dict = Set(i));
+    protected static Func<TTest, Dictionary<Language, Message>> Set;
+    protected static Dictionary<Language, Message> LoadFromFile(string fileName) {
+      return LoadFromJson(ReadUtf8Text(fileName));
+    }
+
+    private static string ReadUtf8Text(string fileName) {
+      var text = File.ReadAllText(GetExecPath(fileName), Encoding.UTF7);
+      return text;
+    }
+
+    /// <summary>
+    /// Generates names for file in every language from Language enum
+    /// </summary>
+    /// <param name="fileNameTemplate">ex: message-{0}.txt</param>
+    /// <returns></returns>
+    protected static Dictionary<Language, Message> LoadFromFiles(string fileNameTemplate) {
+      return FillDict(MakeFileNames(fileNameTemplate));
+    }
+    protected static Dictionary<Language, Message> LoadFromPath(string ext) {
+      return FillDict(EnsureFilePath(ext));
+    }
+
+    private static Dictionary<Language, Message> FillDict(Tuple<Language, string>[] pathes) {
+      return (from x in pathes
+              let text = ReadUtf8Text(x.Item2).ThrowIf(s => string.IsNullOrEmpty(s), "{0}", new { path = x.Item2 })
+              select new { l = x.Item1, m = new Message(text, x.Item2) }
+              ).ToDictionary(x => x.l, x => x.m);
+    }
+
+    protected static Dictionary<Language, Message> LoadFromJson(string json) {
+      return JsonConvert.DeserializeObject<Dictionary<Language, string>>(json)
+        .ToDictionary(kv => kv.Key, kv => new Message(kv.Value, json));
+    }
+    public static Tuple<Language, string>[] MakeFileNames(string nameTemplate) {
+      return GetLanguages().Select(name => Tuple.Create(name, nameTemplate.Formatter(name))).ToArray();
+    }
+
+    private static IEnumerable<Language> GetLanguages() {
+      return Enum.GetValues(typeof(Language)).Cast<Language>();
+    }
+
+    static Tuple<Language, string>[] EnsureFilePath(string ext) {
+      var path = Directory.CreateDirectory(GetExecPath(typeof(TTest).FullName.Replace(".", "\\"))).FullName;
+      Action<string> ensureFile = fileName => {
+        if (!File.Exists(fileName))
+          File.CreateText(fileName).Close();
+      };
+      return GetLanguages()
+        .Select(l => new { l, path = System.IO.Path.Combine(path, l + "." + ext) })
+        .Do(x => ensureFile(x.path))
+        .Select(x => Tuple.Create(x.l, x.path))
+        .ToArray();
+    }
+  }
+}
---
+++ LanguagesTests/LanguagesTests.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HedgeHog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonExtensions;
+using Newtonsoft.Json;
+
+namespace HedgeHog.Tests {
+  static class JsonSrtings {
+    public static string json = @"{
+'en': 'english',
+'es': 'spanisg',
+'pt': 'string'
+}";
+    public static string jsonError = @"{
+'en': 'english',
+'es': 'spanisg',
+'pt': 'string',
+'ru': 'russian'
+}";
+  }
+  public class EMAIL : RunTestable<EMAIL> {
+    public static class LANGS {
+      public class CLICK : Helpers.Languages<CLICK> {
+        static CLICK() {
+          Set = (i) => new Dictionary<Language, Message> {
+              { Language.en,new Message("English","") },
+              { Language.es,new Message("Spanish","") },
+              { Language.pt,new Message("Brazillian","") }
+            };
+        }
+      }
+      public static class JSON_CLASSES {
+        public class JSON : Helpers.Languages<JSON> {
+          static JSON() {
+            Set = (i) => LoadFromJson(JsonSrtings.json);
+          }
+        }
+        public class JSON_ERROR : Helpers.Languages<JSON_ERROR> {
+          static JSON_ERROR() {
+            Set = (i) => LoadFromJson(JsonSrtings.jsonError);
+          }
+        }
+        public class JSON_FILE : Helpers.Languages<JSON_FILE> {
+          static JSON_FILE() {
+            Set = (i) => LoadFromFile("Message.json");
+          }
+        }
+        public class JSON_PATH : Helpers.Languages<JSON_PATH> {
+          static JSON_PATH() {
+            Set = (i) => LoadFromPath("txt");
+          }
+        }
+        public class JSON_PATH_EMPTY : Helpers.Languages<JSON_PATH_EMPTY> {
+          static JSON_PATH_EMPTY() {
+            Set = (i) => LoadFromPath("txt");
+          }
+        }
+        public class JSON_FILE_MALFORM : Helpers.Languages<JSON_FILE_MALFORM> {
+          static JSON_FILE_MALFORM() {
+            Set = (i) => LoadFromFile("MessageError.json");
+          }
+        }
+        public class JSON_FILE_MISSING : Helpers.Languages<JSON_FILE_MISSING> {
+          static JSON_FILE_MISSING() {
+            Set = (i) => LoadFromFile("MessageMissing.json");
+          }
+        }
+        public class JSON_FILES : Helpers.Languages<JSON_FILES> {
+          static JSON_FILES() {
+            Set = (i) => LoadFromFiles("Message-{0}.txt");
+          }
+        }
+      }
+    }
+  }
+
+  [TestClass()]
+  public class LanguagesTests {
+    class DummyLang : Helpers.Languages<DummyLang> { }
+    [TestMethod()]
+    public void LanguageRunTest() {
+      AggregateException errors = new AggregateException();
+      var testResults = EMAIL.RunTests(exc => errors = exc);
+      testResults.ForEach(tr => Console.WriteLine(tr.ToJson()));
+      errors.InnerExceptions.ForEach(error => Console.WriteLine(error.ToMessages() + "\n\n"));
+      Assert.AreEqual(4, errors.InnerExceptions.Count);
+      ExceptionAssert.Propagates<AggregateException>(() => EMAIL.RunTests(), exc => {
+        Assert.AreEqual(4, exc.InnerExceptions.Count);
+      });
+    }
+    [TestMethod()]
+    public void LanguageJson() {
+      var message = EMAIL.LANGS.JSON_CLASSES.JSON.Get(EMAIL.LANGS.JSON_CLASSES.JSON.Language.en);
+      Assert.AreEqual("english", message);
+    }
+    [TestMethod()]
+    public void LanguageFromPath() {
+      var message = EMAIL.LANGS.JSON_CLASSES.JSON_PATH.Get(EMAIL.LANGS.JSON_CLASSES.JSON_PATH.Language.pt);
+      Assert.AreEqual(Encoding.UTF7.GetString(Encoding.Default.GetBytes("questionário")), message);
+    }
+    [TestMethod()]
+    public void LanguageFromPathEmpty() {
+      ExceptionAssert.Propagates<Exception>(() => EMAIL.LANGS.JSON_CLASSES.JSON_PATH_EMPTY.RunTest(), exc => {
+        Assert.IsTrue(exc.InnerException.Message.Contains("pt.txt"), "en.txt not found in exception message");
+      });
+    }
+    [TestMethod()]
+    public void LanguageJsonFromFile() {
+      var message = EMAIL.LANGS.JSON_CLASSES.JSON_FILE.Get(EMAIL.LANGS.JSON_CLASSES.JSON_FILE.Language.en);
+      Assert.AreEqual("english2", message);
+    }
+    [TestMethod()]
+    public void LanguageJsonFromFiles() {
+      var message = EMAIL.LANGS.JSON_CLASSES.JSON_FILES.Get(EMAIL.LANGS.JSON_CLASSES.JSON_FILES.Language.en);
+      Assert.AreEqual("english3", message);
+      message = EMAIL.LANGS.JSON_CLASSES.JSON_FILES.Get(EMAIL.LANGS.JSON_CLASSES.JSON_FILES.Language.es);
+      Assert.AreEqual("spanish3", message);
+    }
+    [TestMethod()]
+    public void LanguageJsonFromMissing() {
+      ExceptionAssert.Propagates<Exception>(() => EMAIL.LANGS.JSON_CLASSES.JSON_FILE_MISSING.RunTest(), exc => {
+        Assert.IsTrue(exc.Message.Contains("JSON_FILE_MISSING"));
+        Assert.AreEqual(typeof(System.IO.FileNotFoundException), exc.InnerException.GetType());
+        Console.WriteLine(exc.ToMessages());
+      });
+    }
+    [TestMethod()]
+    public void LanguageJsonFromFileMalform() {
+      ExceptionAssert.Propagates<Exception>(() => EMAIL.LANGS.JSON_CLASSES.JSON_FILE_MALFORM.RunTest(), exc => {
+        Assert.IsTrue(exc.Message.Contains("JSON_FILE_MALFORM"));
+        Assert.AreEqual(typeof(JsonSerializationException), exc.InnerException.GetType());
+        Console.WriteLine(exc + "");
+      });
+    }
+    [TestMethod()]
+    public void LanguageJsonError() {
+      ExceptionAssert.Propagates<Exception>(() => EMAIL.LANGS.JSON_CLASSES.JSON_ERROR.RunTest(), exc => {
+        Assert.IsTrue(exc.Message.Contains("JSON_ERROR"));
+        Assert.AreEqual(typeof(JsonSerializationException), exc.InnerException.GetType());
+        Console.WriteLine(exc + "");
+      });
+    }
+    [TestMethod()]
+    public void Language() {
+      Assert.AreEqual("English", EMAIL.LANGS.CLICK.Get("en"));
+      Assert.AreEqual("Spanish", EMAIL.LANGS.CLICK.Get(EMAIL.LANGS.CLICK.Language.es));
+      Assert.AreEqual("Brazillian", EMAIL.LANGS.CLICK.Get("português"));
+      ExceptionAssert.Propagates<Exception>(() => EMAIL.LANGS.CLICK.Get("Bla"), exc => {
+        Assert.IsTrue(exc.Message.Contains("Is not mapped"));
+        Assert.IsTrue(exc.Message.Contains("Bla"));
+      });
+    }
+    [TestMethod()]
+    public void MakeFiles() {
+      var names = DummyLang.MakeFileNames("dimok-{0}.txt");
+      names.Select(t => t.Item1).Zip(new[] { DummyLang.Language.en, DummyLang.Language.es, DummyLang.Language.pt }, (e1, e2) => new { e1, e2 }).ForEach(x => Assert.AreEqual(x.e1, x.e2));
+      names.Select(t => t.Item2).Zip(new[] { "dimok-en.txt", "dimok-es.txt", "dimok-pt.txt" }, (e1, e2) => new { e1, e2 }).ForEach(x => Assert.AreEqual(x.e1, x.e2));
+    }
+  }
+}
